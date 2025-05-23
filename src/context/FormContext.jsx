4d32cebd66506c986df09415e7125c460a2fe944
Lucas Lacava <lucaslacava@gmail.com>
2025-05-23 20:25:06 -0300

    add color options to context

--- src/context/FormContext.jsx
+++ src/context/FormContext.jsx
@@ -2,16 +2,32 @@ import { createContext, useContext, useState } from "react";
 
 const FormContext = createContext();
 
+const initialState = {
+  name: "",
+  email: "",
+  color: "",
+  terms: false,
+};
+
 export function FormProvider({ children }) {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    color: "",
-    terms: false,
-  });
+  const [formData, setFormData] = useState(initialState);
+  const [colorOptions, setColorOptions] = useState([]);
+
+  const resetForm = () => {
+    setFormData(initialState);
+    setColorOptions([]);
+  };
 
   return (
-    <FormContext.Provider value={{ formData, setFormData }}>
+    <FormContext.Provider
+      value={{
+        formData,
+        setFormData,
+        resetForm,
+        colorOptions,
+        setColorOptions,
+      }}
+    >
       {children}
     </FormContext.Provider>
   );
