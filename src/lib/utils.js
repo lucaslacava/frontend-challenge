bc82df33357ac62d1548586a32ff1f3c0ea0f198
Lucas Lacava <lucaslacava@gmail.com>
2025-05-26 11:23:04 -0300

    chore: update doc

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,10 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines and merges Tailwind CSS classes using clsx and tailwind-merge.
+ * This utility function helps manage conditional class names and resolves Tailwind class conflicts.
+ */
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
